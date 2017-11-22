c2001400217780ac4586dd0325843612454e58c6
mremondi <mremondi@colby.edu>
2017-11-22 13:09:40 -0500

    navbar ui updates

--- app/src/main/java/curatetechnologies/com/curate/MainActivity.java
+++ app/src/main/java/curatetechnologies/com/curate/MainActivity.java
@@ -103,7 +103,7 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
 
     @Override
     public void onBackPressed() {
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        DrawerLayout drawer = findViewById(R.id.drawer_layout);
         if (drawer.isDrawerOpen(GravityCompat.START)) {
             drawer.closeDrawer(GravityCompat.START);
         } else {
