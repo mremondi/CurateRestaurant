98f1f394a2ba2d12f0c4d1ebc0ce42951fb6edfd
mremondi <mremondi@colby.edu>
2017-11-28 10:50:20 -0500

    Added Manage Menu view with sections

--- app/src/main/java/curatetechnologies/com/curate/controllers/ManageMenu.java
+++ app/src/main/java/curatetechnologies/com/curate/controllers/ManageMenu.java
@@ -3,6 +3,8 @@ package curatetechnologies.com.curate.controllers;
 import android.app.Fragment;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -11,10 +13,13 @@ import android.view.ViewGroup;
 import java.security.acl.LastOwnerException;
 
 import curatetechnologies.com.curate.R;
+import curatetechnologies.com.curate.controllers.adapters.MenuSectionAdapter;
 import curatetechnologies.com.curate.models.Menu;
+import curatetechnologies.com.curate.models.MenuSection;
 import curatetechnologies.com.curate.models.Restaurant;
 import curatetechnologies.com.curate.network.CurateAPI;
 import curatetechnologies.com.curate.network.CurateConnection;
+import io.github.luizgrp.sectionedrecyclerviewadapter.SectionedRecyclerViewAdapter;
 import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
@@ -31,19 +36,30 @@ public class ManageMenu extends Fragment{
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle
             savedInstanceState) {
-        View v = inflater.inflate(R.layout.fragment_select_menu, container, false);
+        View v = inflater.inflate(R.layout.fragment_manage_menu, container, false);
 
         // Todo: load all of the menu's items
         // Todo: allow the restaurant to hide specific items
 
-        getActivity().setTitle("Manage Menu");
+        final RecyclerView recyclerView =  v.findViewById(R.id.manage_menu_recycler_view);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+
 
         final CurateAPI api = CurateConnection.setUpRetrofit();
         Call<Menu> call = api.getMenuById(menuId);
         call.enqueue(new Callback<Menu>() {
             @Override
             public void onResponse(Call<Menu> call, Response<Menu> response) {
+                getActivity().setTitle("Manage Menu: " + response.body().getMenuName());
                 Log.d("MENU LOADED", response.body().getMenuSections().get(0).getSection());
+
+                SectionedRecyclerViewAdapter sectionAdapter = new SectionedRecyclerViewAdapter();
+
+                for (MenuSection section: response.body().getMenuSections()) {
+                    sectionAdapter.addSection(new MenuSectionAdapter(section,null));
+                }
+
+                recyclerView.setAdapter(sectionAdapter);
             }
 
             @Override
---
+++ app/src/main/java/curatetechnologies/com/curate/controllers/adapters/MenuSectionAdapter.java
@@ -0,0 +1,103 @@
+package curatetechnologies.com.curate.controllers.adapters;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import java.util.Arrays;
+import java.util.List;
+
+import curatetechnologies.com.curate.R;
+import curatetechnologies.com.curate.models.MenuItem;
+import curatetechnologies.com.curate.models.MenuSection;
+import io.github.luizgrp.sectionedrecyclerviewadapter.SectionParameters;
+import io.github.luizgrp.sectionedrecyclerviewadapter.StatelessSection;
+
+/**
+ * Created by mremondi on 11/28/17.
+ */
+
+public class MenuSectionAdapter extends StatelessSection {
+
+    MenuSection section;
+    List<String> sectionItems = Arrays.asList("Latte", "Brewed Coffee");
+
+
+    public MenuSectionAdapter(MenuSection section, List<MenuItem> sectionItems){
+        super(new SectionParameters.Builder(R.layout.section_item)
+                .headerResourceId(R.layout.section_header)
+                .build());
+        this.section = section;
+        //this.sectionItems = sectionItems;
+
+    }
+
+    @Override
+    public int getContentItemsTotal() {
+        return sectionItems.size();
+    }
+
+    @Override
+    public RecyclerView.ViewHolder getItemViewHolder(View view) {
+        return new MenuSectionItemViewHolder(view);
+    }
+
+    @Override
+    public void onBindItemViewHolder(RecyclerView.ViewHolder holder, int position) {
+        MenuSectionItemViewHolder itemHolder = (MenuSectionItemViewHolder) holder;
+
+        // bind your view here
+        itemHolder.itemName.setText(this.sectionItems.get(position));
+    }
+
+    @Override
+    public RecyclerView.ViewHolder getHeaderViewHolder(View view) {
+        return new SectionHeaderViewHolder(view);
+    }
+
+    @Override
+    public void onBindHeaderViewHolder(RecyclerView.ViewHolder holder) {
+        SectionHeaderViewHolder headerHolder = (SectionHeaderViewHolder) holder;
+
+        headerHolder.sectionName.setText(this.section.getSection());
+    }
+
+    private class SectionHeaderViewHolder extends RecyclerView.ViewHolder {
+        private final TextView sectionName;
+
+        public SectionHeaderViewHolder(View headerView){
+            super(headerView);
+            sectionName = headerView.findViewById(R.id.section_header_section_name);
+        }
+    }
+
+    private class MenuSectionItemViewHolder extends RecyclerView.ViewHolder {
+        private final TextView itemName;
+        private final Button btnHideItem;
+
+        public MenuSectionItemViewHolder(final View itemView) {
+            super(itemView);
+
+            itemName = itemView.findViewById(R.id.section_item_item_name);
+            btnHideItem = itemView.findViewById(R.id.section_item_hide_item_button);
+
+            btnHideItem.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    // TODO: make a call to the API that makes the menu item hidden
+                    if (btnHideItem.getText().toString().equals("Hide")){
+                        btnHideItem.setText("Unhide");
+                        btnHideItem.setBackgroundColor(itemView.getResources().getColor(R.color.primaryRed));
+                    }
+                    else {
+                        btnHideItem.setText("Hide");
+                        btnHideItem.setBackgroundColor(itemView.getResources().getColor(R.color.greenPastel));
+                    }
+                }
+            });
+        }
+    }
+}
+
+
