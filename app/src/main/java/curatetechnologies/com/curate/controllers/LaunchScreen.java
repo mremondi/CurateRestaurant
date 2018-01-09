0f8d32b20fcc9d06a11f1676c1ffd55da7839692
mremondi <mremondi@colby.edu>
2018-01-09 13:10:32 -0500

    added fabric/crashlytics

--- app/src/main/java/curatetechnologies/com/curate/controllers/LaunchScreen.java
+++ app/src/main/java/curatetechnologies/com/curate/controllers/LaunchScreen.java
@@ -4,16 +4,19 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.crashlytics.android.Crashlytics;
 import com.google.firebase.auth.FirebaseAuth;
 
 import curatetechnologies.com.curate.MainActivity;
+import io.fabric.sdk.android.Fabric;
+import com.crashlytics.android.Crashlytics;
 
 public class LaunchScreen extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
+        Fabric.with(this, new Crashlytics());
 
         FirebaseAuth auth = FirebaseAuth.getInstance();
         if (auth.getCurrentUser() != null) {
