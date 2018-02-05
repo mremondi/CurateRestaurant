8c51305a56c9d1c8ec729a4ae0bb6ca9b3b04ca5
mremondi <mremondi@colby.edu>
2018-02-05 18:06:33 -0500

    small change to link to privacy policy and terms of service

--- app/src/main/java/curatetechnologies/com/curate/controllers/AboutUs.java
+++ app/src/main/java/curatetechnologies/com/curate/controllers/AboutUs.java
@@ -1,12 +1,21 @@
 package curatetechnologies.com.curate.controllers;
 
 import android.app.Fragment;
+import android.content.ActivityNotFoundException;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.Toast;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+import butterknife.Unbinder;
 import curatetechnologies.com.curate.R;
 
 /**
@@ -14,6 +23,10 @@ import curatetechnologies.com.curate.R;
  */
 
 public class AboutUs extends Fragment {
+
+    private Unbinder unbinder;
+
+
 
     @Nullable
     @Override
@@ -23,6 +36,27 @@ public class AboutUs extends Fragment {
 
         getActivity().setTitle("About Us");
 
+        unbinder = ButterKnife.bind(this, v);
         return v;
+    }
+
+    @Override public void onDestroyView() {
+        super.onDestroyView();
+        unbinder.unbind();
+    }
+
+    @OnClick(R.id.btnPrivacyPolicy) void openPrivacyPolicy(){
+        try {
+            Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("http://curatemeals.com/privacypolicy"));
+            getActivity().startActivity(browserIntent);
+        } catch (ActivityNotFoundException e) {
+            Toast.makeText(this.getActivity(), "No application can handle this request. Please install a webbrowser", Toast.LENGTH_LONG).show();
+            e.printStackTrace();
+        }
+    }
+
+    @OnClick(R.id.btnTermsOfService) void termsOfService(){
+        Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("http://www.curatemeals.com/termsofservice"));
+        getActivity().startActivity(browserIntent);
     }
 }
